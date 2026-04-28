25988bd11a14d8f54e187fcdbef347965a347b5d
anderson-estrada-eng <anderpaugamer@gmail.com>
2026-04-28 15:24:40 -0500

    Dia_1_Intento_1

---
+++ Dia_1/is_isogram.cpp
@@ -0,0 +1,76 @@
+//!bool:Si o no
+//Para preparar el código
+#include <utility>
+#include <vector>
+
+std::string(palabra)
+std::cin >> palabra;
+//Para saber si es verdad o falso que no se repiten letras en la palabra.
+bool is_isogram(const std::string& palabra)
+//Para medir la palabra
+    {{
+    length(palabra);
+    for(({
+        palabra(i=0;
+                i<palabra.length();
+                ++i))
+                
+        for(
+            palabra(j;
+                ++j;
+                if (palabra[i] == palabra[j]))))}};
+
+    unsigned(palabra)}
+
+std::pair(palabra)
+if
+    (is_isogram(palabra))
+        std::cout<<"True";
+else
+    std::cout<<"False";
+    }
+return
+    true
+    else
+    false
+    
+    
+
+    
+    
+
+        
+        
+const jdwbn
+if
+    palabra=is_isogram;
+        true
+else
+    palabra=is not_isogram;
+        false
+
+return
+    palabra;
+
+
+
+i
+j
+unsigned
+std::pair
+bool    (     perro)
+isogram
+str::
+s
+i
+j
+++i
+i++
+const
+En:bool is_isogram(const std::string& str)
+length()
+return
+;
+std::pair
+unsigned
+<>
